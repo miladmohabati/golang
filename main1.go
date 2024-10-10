c28ac30372690722d2b4b04fca788700d2712c31
miladmohabati <milad.mohabati.it@gmail.com>
2024-10-10 14:44:37 +0330

    first coding

---
+++ main1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+func main()  {
+     var age int
+	 var name string
+	 var ghad bool
+	 var str float32
+
+	 age=39
+	 name="milad"
+	 ghad=true
+	 str=175.5
+
+
+   
+	fmt.Println(age)
+	fmt.Println(name)
+	fmt.Println(str)
+    fmt.Println((ghad))
+
+
+}
