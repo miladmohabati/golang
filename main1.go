d93f7c546ae7190997a9abbe83e97c7a1ca60a09
miladmohabati <milad.mohabati.it@gmail.com>
2024-10-14 11:21:22 +0330

    change3

--- main1.go
+++ main1.go
@@ -6,11 +6,13 @@ func main()  {
 	 var name string
 	 var ghad bool
 	 var str float32
+	 var sara string
 
 	 age=39
 	 name="milad"
 	 ghad=true
 	 str=175.5
+	 sara="lili ra dost dara"
 
 
    
@@ -18,6 +20,7 @@ func main()  {
 	fmt.Println(name)
 	fmt.Println(str)
     fmt.Println((ghad))
+	fmt.Println(sara)
 
 
 }
