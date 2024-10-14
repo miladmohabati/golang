2d77eac8156055fc7f72df54666cbabb2d120f22
miladmohabati <milad.mohabati.it@gmail.com>
2024-10-14 13:39:07 +0330

    add const

---
+++ main2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	const miladconst string="ninja"
+	miladconst="ninja"
+	var milad string
+	milad="lili"
+	milad="sara"
+
+	fmt.Println(miladconst,milad)
+
+}
